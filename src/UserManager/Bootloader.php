3679accc56cea0e51c2f30c7ed1d2c5435a7c34a
Edmond <1571649+edmonddantes@users.noreply.github.com>
2026-01-16 19:29:35 +0000

    Add success message and fix UserManager bootloader
    - Add success message after deferred tasks completion in package installer
    - Implement BootloaderInterface in UserManager bootloader to fix installation errors

--- src/UserManager/Bootloader.php
+++ src/UserManager/Bootloader.php
@@ -4,4 +4,14 @@ declare(strict_types=1);
 
 namespace IfCastle\Services\UserManager;
 
-class Bootloader {}
+use IfCastle\Application\Bootloader\BootloaderExecutorInterface;
+use IfCastle\Application\Bootloader\BootloaderInterface;
+
+final class Bootloader implements BootloaderInterface
+{
+    #[\Override]
+    public function buildBootloader(BootloaderExecutorInterface $bootloaderExecutor): void
+    {
+        // TODO: Implement UserManager bootloader logic
+    }
+}
